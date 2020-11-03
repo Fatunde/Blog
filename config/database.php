8d269d2c0229605ff47e060c5beb310859528d71
unknown <fatundedamilare@gmail.com>
2020-11-03 15:21:26 +0100

    master

--- config/database.php
+++ config/database.php
@@ -2,7 +2,10 @@
 
 use Illuminate\Support\Str;
 
+$DATABASE_URL=parse_url('postgres://efbfcvgwocuwfy:22273b4f644203f184ee57e9d475d47e94ae1725381870c0e22935c18363b0b1@ec2-107-22-241-205.compute-1.amazonaws.com:5432/d2ucadepnm5mh6');
+
 return [
+    
 
     /*
     |--------------------------------------------------------------------------
@@ -15,7 +18,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'mysql'),
+    'default' => env('DB_CONNECTION', 'pgsql'),
 
     /*
     |--------------------------------------------------------------------------
@@ -65,17 +68,15 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'host' => $DATABASE_URL["host"],
+            'port' => $DATABASE_URL["port"],
+            'database' => ltrim($DATABASE_URL["path"], "/"),
+            'username' => $DATABASE_URL["user"],
+            'password' => $DATABASE_URL["pass"],
             'charset' => 'utf8',
             'prefix' => '',
-            'prefix_indexes' => true,
             'schema' => 'public',
-            'sslmode' => 'prefer',
+            'sslmode' => 'require',
         ],
 
         'sqlsrv' => [
